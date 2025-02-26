98bfd13be091a21146f620459317b2155332b66d
Nan Chen <46341728+nan-v-chen@users.noreply.github.com>
2025-02-26 21:45:48 +0800

    Update EleMi.py

--- EleMi.py
+++ EleMi.py
@@ -7,13 +7,6 @@
 """
 
 import numpy as np
-import logging
-
-
-# use logging module for easy debug
-logging.basicConfig(format='%(asctime)s %(levelname)8s: %(message)s', datefmt='%m-%d %H:%M:%S')
-logger = logging.getLogger(__name__)
-logger.setLevel('DEBUG')
 
 
 def S(alpha, v):
